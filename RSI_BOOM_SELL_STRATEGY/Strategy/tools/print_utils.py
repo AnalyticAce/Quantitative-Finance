89600b481dfd55ab0ccc436fe4b3118fa1513602
DOSSEH Shalom <114688102+AnalyticAce@users.noreply.github.com>
2023-08-07 14:38:12 +0100

    Update print_utils.py

--- RSI_BOOM_SELL_STRATEGY/Strategy/tools/print_utils.py
+++ RSI_BOOM_SELL_STRATEGY/Strategy/tools/print_utils.py
@@ -17,7 +17,20 @@ class printer:
     def help():
         command = " python3 maverick_v3.py run"
         command_ = "python3 maverick_v3.py --telegram"
-        command_t = "python3 maverick_v3.py --telcmd" # to be added (Upcomming)
+        command_t = "python3 maverick_v3.py --telcmd" # to be added as arg (Upcomming)
+        
+        message = (
+            "Hey 🫠 Welcome to Mavery RSI Version 4.0.1\n"
+            f"1. Run {command} to execute the program !\n"
+            f"2. Run {command_} to receive telegram message when a trade is taken \n"
+            f"3. Run {command_t} to see available telegram commands \n"
+            "If you have any other questions or need further assistance, feel free to ask! Happy trading! 📈 \n"
+        )
+
+        playsound("../../Source/son/success.mp3")
+        print(message)
+
+    def command_t():
         
         message = (
             "Hey 🫠 Welcome to Mavery RSI Version 4.0.1\n"
