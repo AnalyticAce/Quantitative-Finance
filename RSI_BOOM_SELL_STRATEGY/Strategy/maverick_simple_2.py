c8474283141b3697ed967d59ffd919f5f88cc160
DOSSEH Shalom <114688102+AnalyticAce@users.noreply.github.com>
2023-08-07 12:25:33 +0100

    Update maverick_simple_2.py

--- RSI_BOOM_SELL_STRATEGY/Strategy/maverick_simple_2.py
+++ RSI_BOOM_SELL_STRATEGY/Strategy/maverick_simple_2.py
@@ -111,6 +111,57 @@ def execute_sell_trade(df, symbol, lot_size=0.2):
         else:
             print("Error executing the trade")
 
+"""
+
+def execute_sell_trade(df, symbol, lot_size=0.2):
+
+    current_bar = df.iloc[-1]
+    previous_bar = df.iloc[-2]
+    confirmation_bar = df.iloc[-3]
+
+    # Check for sell conditions
+    if current_bar["rsi"] > 70 and confirmation_bar["close"] > confirmation_bar["open"] \
+            and previous_bar["close"] < previous_bar["open"] \
+            and current_bar["close"] < current_bar["open"]:
+
+        if not mt5.initialize():
+            print("initialize() failed ☢️")
+            mt5.shutdown()
+            return
+
+        # Execute the trade on the next red candle (Candle 3 in the description)
+        request = {
+            "action": mt5.TRADE_ACTION_DEAL,
+            "symbol": symbol,
+            "volume": lot_size,
+            "type": mt5.ORDER_TYPE_SELL,
+            "price": mt5.symbol_info_tick(symbol).bid,
+            "deviation": 10,
+            "magic": 0,
+            "comment": "RSI Sell Strategy",
+            "type_filling": find_filling_mode(symbol),
+            "type_time": mt5.ORDER_TIME_GTC
+        }
+
+        result = mt5.order_send(request)
+
+        mt5.shutdown()
+
+        if result.comment == "Accepted":
+            print("Sell executed")
+            # Wait for the trade to close (at the close of the second red candle)
+            while True:
+                new_bar = get_historical_data(symbol, mt5.TIMEFRAME_M1, 1)
+                if new_bar is not None:
+                    if new_bar.iloc[0]["close"] < new_bar.iloc[0]["open"]:
+                        break
+                    time.sleep(1)
+            print("Trade closed")
+        else:
+            print("Error executing the trade")
+
+
+"""
 def run_strategy(symbol, timeframe, lot_size=0.2, data_length=1000, period=14):
 
     while True:
