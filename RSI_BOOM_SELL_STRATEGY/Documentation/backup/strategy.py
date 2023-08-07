7d3a1271cfb86e0855ae8186da0fc3589d9c0b14
DOSSEH Shalom <114688102+AnalyticAce@users.noreply.github.com>
2023-08-07 15:22:06 +0100

    Update strategy.py

--- RSI_BOOM_SELL_STRATEGY/Documentation/backup/strategy.py
+++ RSI_BOOM_SELL_STRATEGY/Documentation/backup/strategy.py
@@ -4,8 +4,6 @@ import ta.momentum as momentum
 from datetime import datetime
 import time
 
-# Refer to ../Obselete to add printing telegram feature
-
 def get_historical_data(symbol, timeframe, number_of_data=1000):
     
     """
