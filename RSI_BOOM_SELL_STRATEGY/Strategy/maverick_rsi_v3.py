8e37b56926868056155d3f86fb96d6e87bc6f1fd
DOSSEH Shalom <114688102+AnalyticAce@users.noreply.github.com>
2023-08-08 13:01:16 +0100

    Update maverick_rsi_v3.py

--- RSI_BOOM_SELL_STRATEGY/Strategy/maverick_rsi_v3.py
+++ RSI_BOOM_SELL_STRATEGY/Strategy/maverick_rsi_v3.py
@@ -4,6 +4,7 @@ import ta.momentum as momentum
 from datetime import datetime
 import time
 from Strategy.tools.print_utils import printer, print_status
+import telebot
 # Refer to ../Source/Obselete/maverick_v3 to add printing telegram feature
 
 def get_historical_data(symbol, timeframe, number_of_data=1000):
