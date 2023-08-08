c1c0b9d52c92f7a97fe2b6466c55e4c1bb5973c2
DOSSEH Shalom <114688102+AnalyticAce@users.noreply.github.com>
2023-08-08 14:15:15 +0100

    Create libary.sh

---
+++ RSI_BOOM_SELL_STRATEGY/Documentation/backup/libary.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+packages=(
+    "MetaTrader5==5.0.40"
+    "pandas==1.3.3"
+    "ta==0.7.0"
+    "termcolor==1.1.0"
+    "playsound"
+    "telebot"
+    "pyTelegramBotAPI"
+    "datetime"
+    "plotly"
+)
+
+for package in "${packages[@]}"; do
+    pip install "$package"
+done
+
+echo "Packages installation completed."
