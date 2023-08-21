9d7224371db3d5f4b74ecc18b7ebf5ea9cfa0486
DOSSEH Shalom <114688102+AnalyticAce@users.noreply.github.com>
2023-08-21 19:09:54 +0100

    Update test_1.py

--- RSI_BOOM_SELL_STRATEGY/Documentation/Backup/Boom/test_1.py
+++ RSI_BOOM_SELL_STRATEGY/Documentation/Backup/Boom/test_1.py
@@ -1,13 +1,11 @@
 import MetaTrader5 as mt5
 import pandas as pd
 import ta.momentum as momentum
-from datetime import datetime, timedelta
+from datetime import datetime
 import time
 
-## Take profit is the close of the candle 
+def get_historical_data(symbol, timeframe, number_of_data=1000):
 
-def get_historical_data(symbol, timeframe, number_of_data):
-    
     if not mt5.initialize():
         print("initialize() failed ☢️")
         mt5.shutdown()
@@ -28,7 +26,7 @@ def get_historical_data(symbol, timeframe, number_of_data):
 
     return df
 
-def calculate_rsi(df, period):
+def calculate_rsi(df, period=14):
 
     try:
         rsi_indicator = momentum.RSIIndicator(df["close"], window=period)
@@ -36,34 +34,13 @@ def calculate_rsi(df, period):
     except Exception as e:
         print(f"Error calculating RSI: {e}")
 
-def find_filling_mode(symbol):
-
-    for i in range(2):
-
-        request = {
-            "action": mt5.TRADE_ACTION_DEAL,
-            "symbol": symbol,
-            "volume": mt5.symbol_info(symbol).volume_min,
-            "type": mt5.ORDER_TYPE_BUY,
-            "price": mt5.symbol_info_tick(symbol).ask,
-            "type_filling": i,
-            "type_time": mt5.ORDER_TIME_GTC
-        }
-
-        result = mt5.order_check(request)
-
-        if result.comment == "Done":
-            print("Trade was closed")
-            break
-
-    return i
-
-def execute_sell_trade(df, symbol, lot_size, take_profit):
+def execute_sell_trade(df, symbol, lot_size=0.2):
 
     current_bar = df.iloc[-1]
     previous_bar = df.iloc[-2]
     confirmation_bar = df.iloc[-3]
 
+    # Check for sell conditions
     if current_bar["rsi"] > 70 and confirmation_bar["close"] > confirmation_bar["open"] \
             and previous_bar["close"] < previous_bar["open"] \
             and current_bar["close"] < current_bar["open"]:
@@ -73,37 +50,61 @@ def execute_sell_trade(df, symbol, lot_size, take_profit):
             mt5.shutdown()
             return
 
-        entry_price = mt5.symbol_info_tick(symbol).bid
-
-        # Calculate the take-profit level as the close of the current candle
-        take_profit_price = current_bar["close"]
-
+        # Execute the trade on the next red candle (Candle 3 in the description)
         request = {
             "action": mt5.TRADE_ACTION_DEAL,
             "symbol": symbol,
             "volume": lot_size,
             "type": mt5.ORDER_TYPE_SELL,
-            "price": entry_price,
+            "price": mt5.symbol_info_tick(symbol).bid,
             "deviation": 0,
             "magic": 0,
             "comment": "RSI Sell Strategy",
-            "type_filling": find_filling_mode(symbol),
-            "type_time": mt5.ORDER_TIME_GTC,
-            "sl": 0,  # No stop-loss
-            "tp": take_profit_price  # Set the take-profit level
+            "type_filling": mt5.ORDER_FILLING_IOC,
+            "type_time": mt5.ORDER_TIME_GTC
         }
 
         result = mt5.order_send(request)
 
         mt5.shutdown()
 
-        if result.comment == "Accepted":
-            print(f"Sell executed with take-profit at {take_profit_price}")
+        if result.comment == "Accepted": 
+            print("Sell executed") 
+            time.sleep(60)
+            
+            new_bar = get_historical_data(symbol, mt5.TIMEFRAME_M1, 1)
+            
+            #if new_bar is not None and new_bar.iloc[0]["open"] < new_bar.iloc[0]["close"]:
+            if new_bar is not None and new_bar.iloc[0]["close"] < new_bar.iloc[0]["open"]:
+                close_price = mt5.symbol_info_tick(symbol).ask
+        
+                request_close = {
+                    "action": mt5.TRADE_ACTION_DEAL,
+                    "symbol": symbol,
+                    "volume": lot_size,
+                    "type": mt5.ORDER_TYPE_BUY,
+                    "price": close_price,
+                    "deviation": 0,
+                    "magic": 0,
+                    "comment": "Close Trade",
+                    "type_filling": mt5.ORDER_FILLING_IOC,
+                    "type_time": mt5.ORDER_TIME_GTC
+                }
+                
+                result_close = mt5.order_send(request_close)
+                
+                if result_close.comment == "Accepted":
+                    print("Trade closed at open of the next candle")
+                else:
+                    print("Error closing the trade")
+            else:
+                print("No trade closure condition met (next candle is not bullish)")
         else:
-            print("Error closing the trade")
+            print("Error executing the trade")
 
-def run_strategy(symbol, timeframe, lot_size, data_length, period):
-    
+
+def run_strategy(symbol, timeframe, lot_size=0.2, data_length=1000, period=14):
+
     while True:
 
         try:
@@ -122,10 +123,13 @@ def run_strategy(symbol, timeframe, lot_size, data_length, period):
         time.sleep(sleep_duration)
 
 if __name__ == "__main__":
+
     symbol = "Boom 1000 Index"
     timeframe = mt5.TIMEFRAME_M1
+
     lot_size = 1.0
-    data_length = 500
+
+    data_length = 1000
     period = 7
 
     run_strategy(symbol, timeframe, lot_size, data_length, period)
