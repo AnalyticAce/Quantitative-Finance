e38ddc5562c6915d69aeca2f52aeb960c7fb7a38
DOSSEH Shalom <shalom.dosseh@epitech.eu>
2023-09-05 22:51:32 +0100

    [add] fectching crypto data using ccxt

--- Statistical-Arb-Oculus/src/data/crypto_data.py
+++ Statistical-Arb-Oculus/src/data/crypto_data.py
@@ -1,23 +1,24 @@
 import ccxt
 import pandas as pd
-from secret import binance
+import matplotlib.pyplot as plt  # Import pyplot from matplotlib
 
 symbol = "BTC/USDT"
-timeframe = "15m"
+timeframe = "1m"  # Changed to "1d" for daily data
 limit = 1000
 
 def fetch_crypto_data(symbol, timeframe, limit):
-        
     binance = ccxt.binance({
-        "enabledRateLimit" : True,
+        "enabledRateLimit": True,
         "apiKey": "0phrfi8fe83w2IRtumAvYaTWzh43MiGwGyePDxZx9HCWloEUUpuMzTLOWlclxK9y",
-        "secret" : "2uY1G46lIg8abkslJsOrVLYOIqFSv1OP0PSKkuRm05e6HALefCpZGhXBQZuyYQIz"
+        "secret": "2uY1G46lIg8abkslJsOrVLYOIqFSv1OP0PSKkuRm05e6HALefCpZGhXBQZuyYQIz"
     })
 
-    bars = binance.fetch_ohlcv(symbol, timeframe = timeframe, limit = limit)
+    bars = binance.fetch_ohlcv(symbol, timeframe=timeframe, limit=limit)
 
     df = pd.DataFrame(bars, columns=["timestamp", "open", "high", "low", "close", "volume"])
 
     df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
 
-    return df[["timestamp", "close"]]
+    return df
+
+#c = fetch_crypto_data(symbol, timeframe, limit)
