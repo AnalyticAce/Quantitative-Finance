87a3b2c162c6faf6fb0a5ad99443708888def650
DOSSEH Shalom <shalom.dosseh@epitech.eu>
2023-09-05 23:19:31 +0100

    [add] rearrange files

--- Statistical-Arb-Oculus/src/data/fetch_data.py
+++ Statistical-Arb-Oculus/src/data/fetch_data.py
@@ -1,8 +1,11 @@
 import ccxt
 import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
+import tradermade as tm
 
 def fetch_crypto_data(symbol, timeframe, limit):
+    
     binance = ccxt.binance({
         "enabledRateLimit": True,
         "apiKey": "0phrfi8fe83w2IRtumAvYaTWzh43MiGwGyePDxZx9HCWloEUUpuMzTLOWlclxK9y",
@@ -15,4 +18,18 @@ def fetch_crypto_data(symbol, timeframe, limit):
 
     df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
 
+    return df
+
+def fetch_fx_data(symbol, start_date, end_date, timeframe):
+    
+    tm.set_rest_api_key("PQgteXR13COKO54qQXdH")
+    
+    data = tm.timeseries(
+        currency = symbol, 
+        start = start_date, 
+        end= end_date, 
+        interval = timeframe ,
+        fields=["open", "high", "low","close"]
+    )
+    df = pd.DataFrame(data)
     return df
