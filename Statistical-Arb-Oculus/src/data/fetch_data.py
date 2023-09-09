654361eecf02808ead1b62103afcf303ebe4dc36
DOSSEH Shalom <shalom.dosseh@epitech.eu>
2023-09-09 21:29:01 +0100

    [del] useless files

--- Statistical-Arb-Oculus/src/data/fetch_data.py
+++ Statistical-Arb-Oculus/src/data/fetch_data.py
@@ -5,7 +5,6 @@ import pandas as pd
 import tradermade as tm
 
 def fetch_crypto_data(symbol, timeframe, limit):
-    # Create a Binance instance
     binance = ccxt.binance()
 
     try:
@@ -29,10 +28,8 @@ def fetch_fx_data(symbol, start_date, end_date, timeframe):
     
     tm.set_rest_api_key("PQgteXR13COKO54qQXdH")
     
-    # print the type of symbol
     print(type(symbol))
     
-    # pass symbol as it is
     data = tm.timeseries(
         currency = symbol,
         start = start_date, 
@@ -40,24 +37,11 @@ def fetch_fx_data(symbol, start_date, end_date, timeframe):
         interval = timeframe,
         fields=["open", "high", "low", "close"]
     )
-    #df = pd.DataFrame(data)
-    
-    # assign a value to currency
+
     currency = symbol
     
-    # print the type of currency
     print(type(currency))
-    
-    # use str.split() method on currency
+
     data = currency.str.split(",")
     
     return data
-
-fetch_crypto_data("BTC/USD", "1d", 1000)
-fetch_crypto_data("ETH/EUR", "1d", 1000)
-#forex_symbol = "EURUSD"
-#forex_start_date = "2023-01-01"
-#forex_end_date = "2023-09-01"
-#forex_timeframe = "daily"
-#forex_data = fetch_fx_data(forex_symbol, forex_start_date, forex_end_date, forex_timeframe)
-#print(forex_data.head())
