42bae8d0998b1c8b71015e56117873ca7ad0c20b
DOSSEH Shalom <shalom.dosseh@epitech.eu>
2023-09-09 21:04:46 +0100

    [add] change calculate_correlation function

--- Statistical-Arb-Oculus/src/mectrics/crypto_mectrics.py
+++ Statistical-Arb-Oculus/src/mectrics/crypto_mectrics.py
@@ -6,26 +6,21 @@ import matplotlib.pyplot as plt
 from statsmodels.tsa.stattools import adfuller
 import statsmodels.api as sm
 
-def calculate_correlation(symbol_1, symbol_2):
+def calculate_correlation(closing_prices_asset_1, closing_prices_asset_2):
     """
-    Calculate the correlation coefficient between two assets.
+    Calculate the correlation coefficient between two assets based on their closing prices.
     
     Args:
-        symbol_1 (str): CSV file path for the first asset's data.
-        symbol_2 (str): CSV file path for the second asset's data.
+        closing_prices_asset_1 (pandas.Series): Closing prices of the first asset.
+        closing_prices_asset_2 (pandas.Series): Closing prices of the second asset.
     
     Returns:
         float: The correlation coefficient between the closing prices of the two assets.
     """
-    asset_1 = pd.DataFrame(pd.read_csv(symbol_1))
-    asset_2 = pd.DataFrame(pd.read_csv(symbol_2))
-
-    closing_prices_asset_1 = asset_1["close"]
-    closing_prices_asset_2 = asset_2["close"]
-
     correlation_coefficient, _ = pearsonr(closing_prices_asset_1, closing_prices_asset_2)
     
     return correlation_coefficient
+
 
 def calculate_spread(closing_prices_asset_1, closing_prices_asset_2):
     """
@@ -98,27 +93,3 @@ def is_stationary(spread, significance_level=0.05):
         return True
     else:
         return False
-
-
-df_1 = pd.read_csv("../../data/btc_usd.csv")
-df_2 = pd.read_csv("../../data/eth_usd.csv")
-asset_1 = pd.DataFrame(df_1)
-asset_2 = pd.DataFrame(df_2)
-
-closing_prices_asset_1 = asset_1["close"]
-closing_prices_asset_2 = asset_2["close"]
-
-spread = calculate_spread(closing_prices_asset_1, closing_prices_asset_2)
-
-is_stationary_result = is_stationary(spread)
-
-if is_stationary_result:
-    print("The spread is stationary.")
-else:
-    print("The spread is not stationary.")
-
-z_scores = calculate_zscore(spread)
-print("Z-Scores:", z_scores)
-
-hedge_r = hedge_ratio(closing_prices_asset_2, closing_prices_asset_1)
-print(f"Hedge Ratio: {hedge_r:.2f}")
